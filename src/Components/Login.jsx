0d73bde91ddc8b24ee1e992b782e3cc5563fff8b
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-05-26 10:33:40 -0700

    create base theme and copyrightFunction

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+
+
+const theme = createTheme();
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
   )
 }
+
+function Copyright(props) {
+    return (
+      <Typography
+        variant="body2"
+        color="text.secondary"
+        align="center"
+        {...props}
+      >
+        {"Copyright © "}
+        <Link color="inherit" href="http://localhost:3000/">
+          Class Project
+        </Link>{" "}
+        {new Date().getFullYear()}
+        {"."}
+      </Typography>
+    );
+  }
