33f3b3152c5ebd9d26ec6877deb2a0455f3339da
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-05-27 06:36:43 -0700

    complete grid system for page

--- src/Components/Owner.jsx
+++ src/Components/Owner.jsx
@@ -1,5 +1,17 @@
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+
 export default function Owner() {
   return (
-    <div>Owner</div>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container columns={12}>
+        <Grid item xs={12} sm={4}>
+         
+        </Grid>
+        <Grid item xs={12} sm={8} sx={{ background: "gold" }}>
+
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
