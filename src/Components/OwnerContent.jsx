653c32b18fa192533176e0c10436f3928b57bd38
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-05-27 17:02:26 -0700

    filter persons and delete target from data

--- src/Components/OwnerContent.jsx
+++ src/Components/OwnerContent.jsx
@@ -1,9 +1,28 @@
 import List from "@mui/material/List";
+import PersonInfo from "./PersonInfo";
 
 export default function OwnerContent() {
+  const deleteHandler = (id) => {
+    console.log(fakeData.filter((person) => person.id !== id));
+  };
+
   return (
     <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-      
+      {fakeData?.map((info, index) => (
+        <PersonInfo
+          key={index}
+          OnDelete={deleteHandler}
+          Name={info.name}
+          ID={info.id}
+        />
+      ))}
     </List>
   );
 }
+
+const fakeData = [
+  { name: "RC", id: "1" },
+  { name: "Mahdi", id: "2" },
+  { name: "Fateme", id: "3" },
+  { name: "Vesal", id: "4" },
+];
