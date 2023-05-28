254abdd06b557b4d718f7036dcad4d5303af84dd
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-05-27 18:13:19 -0700

    create modal for add person

---
+++ src/Components/AddPerson.jsx
@@ -0,0 +1,53 @@
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+
+import { useState } from "react";
+
+export default function AddPerson({ title }) {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button
+        variant="outlined"
+        sx={{ width: "280px", borderRadius: "15px", letterSpacing: "2px" }}
+        endIcon={<PersonAddIcon />}
+        onClick={handleClickOpen}
+      >
+        {title}
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>{title}</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Name"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Add</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
