dc30bd014bb72fedcdc937faba678212d1da935e
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-05-30 10:49:28 -0700

    add list text and btn delete to student

--- src/Students.jsx
+++ src/Students.jsx
@@ -1,11 +1,15 @@
 import ListItem from "@mui/material/ListItem";
 import Divider from "@mui/material/Divider";
+import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
 import { Box } from "@mui/material";
 
-export default function Student({ Name, AvatarSrc = "",}) {
- 
+export default function Student({ Name, AvatarSrc = "", ID}) {
+
   return (
     <Box sx={{ m: 2 }}>
       <ListItem
@@ -19,7 +23,41 @@ export default function Student({ Name, AvatarSrc = "",}) {
         <ListItemAvatar>
           <Avatar alt={Name} src={AvatarSrc} />
         </ListItemAvatar>
-        
+        <ListItemText
+          secondary={
+            <>
+              <Typography
+                sx={{
+                  display: "inline",
+                  fontWeight: "bold",
+                  fontStyle: "italic",
+                  fontSize: "16px",
+                  color: "#666",
+                }}
+                component="span"
+                variant="overline"
+                color="text.primary"
+              >
+                {Name}
+              </Typography>
+            </>
+          }
+        />
+        <Button
+          sx={{
+            display: "flex",
+            lineHeight: "30px",
+            borderBottom: "1px solid",
+            borderRadius: "0px",
+          }}
+          size="small"
+          endIcon={<DeleteIcon />}
+          color="secondary"
+          onClick={deleteHandler}
+          id={ID}
+        >
+          Delete
+        </Button>
       </ListItem>
       <Divider variant="inset" component="li" />
     </Box>
