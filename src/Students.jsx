4a05561a5a51249b594dcf51de94f3693be9ebeb
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-05-30 10:48:31 -0700

    add list item and avatar for student

--- src/Students.jsx
+++ src/Students.jsx
@@ -1,10 +1,27 @@
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
 import { Box } from "@mui/material";
 
-export default function Student() {
-  
+export default function Student({ Name, AvatarSrc = "",}) {
+ 
   return (
     <Box sx={{ m: 2 }}>
-     
+      <ListItem
+        sx={{
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
+          width: 360,
+        }}
+      >
+        <ListItemAvatar>
+          <Avatar alt={Name} src={AvatarSrc} />
+        </ListItemAvatar>
+        
+      </ListItem>
+      <Divider variant="inset" component="li" />
     </Box>
   );
 }
