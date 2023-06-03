b50087d11e520de0e04f0558803f58b408acf19a
NahidHadizadeh <nahidhadizadeh7@gmail.com>
2023-06-03 23:58:49 +0430

    Add page folder

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "./Components/Login";
-import Owner from "./Components/Owner";
-import Teacher from "./Components/Teacher";
-import PrivateRoutes from "./Utils/PrivateRoutes"
+import Login from "./Pages/Login";
+import Owner from "./Pages/Owner";
+import Teacher from "./Pages/TeacherPage";
+import Student from "./Pages/StudentPage";
+import PrivateRoutes from "./Utils/PrivateRoutes";
 import { Auth } from "./Utils/Auth";
 
 function App() {
   return (
     <Auth>
       <Routes>
-        <Route path="/Owner" element={
-          <PrivateRoutes  >
-            <Owner />
-          </PrivateRoutes>
-        }></Route>
-        <Route path="/Teacher" element={
-          <PrivateRoutes  >
-            <Teacher />
-          </PrivateRoutes>
-        }></Route>
+        <Route
+          path="/Owner"
+          element={
+            <PrivateRoutes>
+              <Owner />
+            </PrivateRoutes>
+          }
+        ></Route>
+        <Route
+          path="/Teacher"
+          element={
+            <PrivateRoutes>
+              <Teacher />
+            </PrivateRoutes>
+          }
+        ></Route>
 
-        {/* <Route path="/Student" element={
-          <PrivateRoutes  >
-            <Student />
-          </PrivateRoutes>
-        }></Route> */}
+        <Route
+          path="/Student"
+          element={
+            <PrivateRoutes>
+              <Student />
+            </PrivateRoutes>
+          }
+        ></Route>
 
         <Route element={<Login />} path="/" />
       </Routes>
