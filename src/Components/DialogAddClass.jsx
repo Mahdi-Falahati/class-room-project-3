4159c46fccb23cf16562a616087c4799f17650e2
NahidHadizadeh <nahidhadizadeh7@gmail.com>
2023-06-04 16:41:24 +0430

    add modal for add new class

---
+++ src/Components/DialogAddClass.jsx
@@ -0,0 +1,78 @@
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import GroupAddIcon from "@mui/icons-material/GroupAdd";
+import Typography from "@mui/material/Typography";
+
+import { useState } from "react";
+
+export default function DialogAddClass({
+  title,
+  Organization,
+  Class,
+  icon,
+  user,
+}) {
+  const [open, setOpen] = useState(false);
+  const [term, setTerm] = useState("");
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleTerm = (e) => {
+    setTerm(e.target.value);
+  };
+
+  const handleAddPerson = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div style={{ margin: "10px" }}>
+      <Button
+        variant="outlined"
+        sx={{ width: "280px", borderRadius: "15px", letterSpacing: "2px" }}
+        endIcon={icon}
+        onClick={handleClickOpen}
+      >
+        {title}
+      </Button>
+
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle sx={{ display: "flex", alignItems: "center" }}>
+          {title} <GroupAddIcon sx={{ marginLeft: "5px" }} />
+        </DialogTitle>
+
+        <DialogContent sx={{ width: "300px" }}>
+          <TextField
+            autoFocus
+            onChange={handleTerm}
+            value={term}
+            margin="dense"
+            id="titleClass"
+            label="Title"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+
+          <Typography variant="overline" display="block" gutterBottom>
+            {Organization} - {Class}
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleAddPerson}>Add</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
