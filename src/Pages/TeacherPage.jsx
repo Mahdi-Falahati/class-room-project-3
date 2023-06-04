62dc95ccbc053e57488852108c1429633f16a5c0
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-06-04 01:44:05 -0700

    Delete codes that were not used

--- src/Pages/TeacherPage.jsx
+++ src/Pages/TeacherPage.jsx
@@ -1,27 +1,8 @@
-import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import SubHeader from "./SubHeader";
 import SelectOptions from "./SelectOptions";
 
 export default function Teacher() {
-  const defaultPropsOrganistion = {
-    options: Organistion,
-    getOptionLabel: (option) => option,
-  };
-
-  const defaultPropsClasses = {
-    options: Classes,
-    getOptionLabel: (option) => option,
-  };
-
-  const organistionValueHandler = (e) => {
-    console.log(e.target.innerText);
-  };
-
-  const ClassesValueHandler = (e) => {
-    console.log(e.target.innerText);
-  };
-
   return (
     <Box sx={{ flexGrow: 1 }}>
       <SubHeader user="teacher" />
