10ddf102a38d420993fcb7d4734e8a7964a9589f
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-06-06 02:04:03 -0700

    manage data

--- src/Utils/Store/StoreContext.jsx
+++ src/Utils/Store/StoreContext.jsx
@@ -1,10 +1,15 @@
-import { createContext,  } from "react";
+import { createContext, useState } from "react";
+
 export const StoreContext = createContext();
 
 export function StoreProvider({ children }) {
-  
+  const [data, setData] = useState([]);
+
+  const updateData = (newData) => {
+    setData(newData);
+  };
   return (
-    <StoreContext.Provider>
+    <StoreContext.Provider value={{ data, updateData }}>
       {children}
     </StoreContext.Provider>
   );
