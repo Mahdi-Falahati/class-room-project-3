705baf1c46f4255c36c51f7fc33335cc2e541b28
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-06-06 04:03:03 -0700

    update Store add select class state

--- src/Utils/Store/StoreContext.jsx
+++ src/Utils/Store/StoreContext.jsx
@@ -4,12 +4,24 @@ export const StoreContext = createContext();
 
 export function StoreProvider({ children }) {
   const [data, setData] = useState([]);
+  const [selectClass, setSelectClass] = useState("");
+
+  const updateSelectClass = (newClass) => {
+    setSelectClass(newClass);
+  };
 
   const updateData = (newData) => {
     setData(newData);
   };
   return (
-    <StoreContext.Provider value={{ data, updateData }}>
+    <StoreContext.Provider
+      value={{
+        data,
+        selectClass,
+        updateData,
+        updateSelectClass,
+      }}
+    >
       {children}
     </StoreContext.Provider>
   );
