f3c326828c7d92992165c3ad5f0bffbe802e6051
NahidHadizadeh <nahidhadizadeh7@gmail.com>
2023-06-06 20:28:28 +0430

    using "ClassInfo" instead of "PersonInfo"

--- src/Components/DialogEditClass.jsx
+++ src/Components/DialogEditClass.jsx
@@ -4,16 +4,53 @@ import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
-import AssignmentTurnedInIcon from "@mui/icons-material/AssignmentTurnedIn";
-import AssistantIcon from "@mui/icons-material/Assistant";
 import EditIcon from "@mui/icons-material/Edit";
-import { useState } from "react";
-import { Typography } from "@mui/material";
+import { useReducer, useState } from "react";
+import { Autocomplete, Typography } from "@mui/material";
 import style from "./btn.module.css";
 
-export default function DialogEditClass({ Organization, Class, title }) {
+export default function DialogEditClass({ Organization = "aaa", title }) {
   const [open, setOpen] = useState(false);
-  const [term, setTerm] = useState(title ? title : "");
+  const Title = title ? title : "";
+
+  const [value, setValue] = useState(teacherFake[0]);
+  const [inputValue, setInputValue] = useState("");
+  const [error, setError] = useState({
+    classTitle: false,
+    teacher: false,
+  });
+
+  // ---------------reduser
+  const ActionType = {
+    ClassTitle: "__ClassTitle",
+    Teacher: "__Teacher",
+  };
+
+  const formReducer = (state, action) => {
+    switch (action.type) {
+      case ActionType.ClassTitle:
+        return { ...state, classTitle: action.value };
+      case ActionType.Teacher:
+        return { ...state, teacher: action.value };
+      default:
+        break;
+    }
+  };
+
+  const initialValue = {
+    classTitle: Title,
+    teacher: "",
+  };
+  const [data, dispatchInputData] = useReducer(formReducer, initialValue);
+
+  const classTitleHandler = (e) => {
+    dispatchInputData({ type: ActionType.ClassTitle, value: e.target.value });
+  };
+  const selectTeacherHandler = (e, newValue) => {
+    setValue(newValue);
+    console.log(e.target.value);
+    dispatchInputData({ type: ActionType.Teacher, value: newValue });
+  };
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -23,12 +60,22 @@ export default function DialogEditClass({ Organization, Class, title }) {
     setOpen(false);
   };
 
-  const handleTerm = (e) => {
-    setTerm(e.target.value);
+  const handleEditClass = () => {
+    if (data.classTitle.trim() && data.teacher) {
+      setError({ classTitle: false, teacher: false });
+      setOpen(false);
+    } else if (!data.classTitle.trim()) {
+      setError({ classTitle: true, teacher: false });
+    } else if (!data.teacher) {
+      setError({ classTitle: false, teacher: true });
+    } else {
+      setError({ classTitle: false, teacher: false });
+      setOpen(false);
+    }
   };
-
-  const handleAssignGradeToHomeWrok = () => {
-    setOpen(false);
+  const defaultPropsTeacher = {
+    options: teacherFake,
+    getOptionLabel: (option) => option,
   };
 
   return (
@@ -38,40 +85,57 @@ export default function DialogEditClass({ Organization, Class, title }) {
         className={style.editBtn}
         onClick={handleClickOpen}
       >
-        {title ? <EditIcon /> : <AssignmentTurnedInIcon />}
+        <EditIcon />
       </Button>
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>
           Edit Class
-          <AssistantIcon sx={{ marginLeft: "5px" }} />
+          <EditIcon sx={{ marginLeft: "5px" }} />
         </DialogTitle>
         <DialogContent>
-          {/* ---------------- if edit Homework , titleHw === "" */}
-          {!title ? (
-            ""
-          ) : (
-            <TextField
-              autoFocus
-              onChange={handleTerm}
-              value={term}
-              margin="dense"
-              id="name"
-              label="title"
-              type="string"
-              fullWidth
-              variant="standard"
-            />
-          )}
+          <TextField
+            onChange={classTitleHandler}
+            error={error.classTitle}
+            value={data.classTitle}
+            autoFocus
+            margin="dense"
+            id="classTitle"
+            label="title"
+            type="string"
+            fullWidth
+            variant="standard"
+          />
+          <Autocomplete
+            value={value}
+            inputValue={inputValue}
+            onInputChange={(event, newInputValue) => {
+              setInputValue(newInputValue);
+            }}
+            onChange={(e, newValue) => selectTeacherHandler(e, newValue)}
+            sx={{ mb: 1, width: "280px" }}
+            {...defaultPropsTeacher}
+            id="auto-highlight-teacher"
+            autoHighlight
+            renderInput={(params) => (
+              <TextField
+                error={error.teacher}
+                {...params}
+                label="Teacher"
+                variant="standard"
+              />
+            )}
+          />
 
           <Typography variant="overline" display="block" gutterBottom>
-            {Organization} - {Class}
+            org: {Organization}
           </Typography>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleAssignGradeToHomeWrok}>Edit</Button>
+          <Button onClick={handleEditClass}>Edit</Button>
         </DialogActions>
       </Dialog>
     </div>
   );
 }
+const teacherFake = ["T111", "T222", "T333"];
