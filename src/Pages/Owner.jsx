1d876dc38f62f14ca49278d435cbc91e00c09746
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-06-06 02:19:54 -0700

    get data from store context and send data to select option component

--- src/Pages/Owner.jsx
+++ src/Pages/Owner.jsx
@@ -1,12 +1,16 @@
 import Box from "@mui/material/Box";
 import SubHeader from "../Components/SubHeader";
 import SelectOptions from "../Components/SelectOptions";
+import { useContext } from "react";
+import { StoreContext } from "../Utils/Store/StoreContext";
 
 export default function Owner() {
+  const { data } = useContext(StoreContext);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <SubHeader user="Admin" />
-      <SelectOptions user={"Admin"} />
+      <SelectOptions user={"Admin"} organizations={data.organizations} />
     </Box>
   );
 }
