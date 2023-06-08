557d1abbbeeff596d3751d93e24779bb29c9fd44
NahidHadizadeh <nahidhadizadeh7@gmail.com>
2023-06-08 09:05:31 +0430

    create route for signUp page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Login from "./Pages/Login";
 import Owner from "./Pages/Owner";
 import Teacher from "./Pages/TeacherPage";
 import Student from "./Pages/StudentPage";
+import SignUp from "./Pages/SignUp";
 import PrivateRoutes from "./Utils/PrivateRoutes";
 import { Auth } from "./Utils/Auth";
 
@@ -32,6 +33,14 @@ function App() {
           element={
             <PrivateRoutes>
               <Student />
+            </PrivateRoutes>
+          }
+        ></Route>
+        <Route
+          path="/SignUp"
+          element={
+            <PrivateRoutes>
+              <SignUp />
             </PrivateRoutes>
           }
         ></Route>
