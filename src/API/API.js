25f588e4c4fff28dc5767eba262ff0147e0e4cb0
NahidHadizadeh <nahidhadizadeh7@gmail.com>
2023-06-08 11:56:48 +0430

    add createOwner for sign up Admin

--- src/API/API.js
+++ src/API/API.js
@@ -1,36 +1,44 @@
 import axios from "axios";
 const baseURL = "http://localhost:5000";
 const getOrganOwner = async (path, info) => {
-    try {
-        const result = await axios.post(baseURL + path, info);
-        return result.data;
-
-    } catch (error) {
-        console.log(error);
-    }
-}
-const getTeacher = async (path, info) => {
-    try {
-        const result = await axios.post(baseURL + path, info)
-        return result.data
-    } catch (error) {
-        console.log(error);
-    }
-}
-const getStudent = async (path, info) => {
-    try {
-        const result = await axios.post(baseURL + path, info)
-        return result.data
-    } catch (error) {
-        console.log(error);
-    }
-}
-const getOrganization = async (path, id) => {
-    try {
-        const result = await axios.post(baseURL + path, id);
-        return result.data;
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const result = await axios.post(baseURL + path, info);
+    return result.data;
+  } catch (error) {
+    console.log(error);
+  }
 };
-export { getOrganOwner, getTeacher, getStudent, getOrganization }
+const getTeacher = async (path, info) => {
+  try {
+    const result = await axios.post(baseURL + path, info);
+    return result.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+const getStudent = async (path, info) => {
+  try {
+    const result = await axios.post(baseURL + path, info);
+    return result.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+const getOrganization = async (path, id) => {
+  try {
+    const result = await axios.post(baseURL + path, id);
+    return result.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+const createOwner = async (path, info) => {
+  try {
+    const result = await axios.post(baseURL + path, info);
+    return result.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { getOrganOwner, getTeacher, getStudent, getOrganization, createOwner };
